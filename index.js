973dbd71b3a9f3b201e4735d14cd3fc2965c253a
MohammedAlMaqbli <Mohammed.AlMaqbli@gmail.com>
2019-09-26 20:07:32 -0700

    Done

--- index.js
+++ index.js
@@ -8,4 +8,28 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  time = time.split(":");
+  time = parseInt(time , 10);
+
+  if (time < 12 ) {
+    return "Good Morning";
+  }
+  if (time >= 12 && time < 17){
+
+    return "Good Afternoon";
+  }
+  if (time >= 17 && time < 24){
+    return "Good Evening";
+  }
+  
+  return "Error";
+}
+
+  
 /* Write your implementation of displayMessage() */
+function displayMessage(greet){
+  const element = document.getElementById('greeting');
+  element.innerText='greet';
+  
+}
