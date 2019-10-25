8406de10d05130071f4e0dbddce015bc13fc0b8a
Firas Al-Azizy <firasazizy614@gmail.com>
2019-10-24 23:52:54 -0700

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,28 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(time) {
+  time = time.split(":");
+  time = parseInt(time , 10);
+
+  if (time < 12 ) {
+    return "Good Morning";
+  }
+  if (time >= 12 && time < 17){
+
+    return "Good Afternoon";
+  }
+  if (time >= 17 && time < 24){
+    return "Good Evening";
+  }
+  
+  return "Error";
+}
+
+  
+/* Write your implementation of displayMessage() */
+function displayMessage(greet){
+  const element = document.getElementById('greeting');
+  element.innerText=greet;
+  
+}
