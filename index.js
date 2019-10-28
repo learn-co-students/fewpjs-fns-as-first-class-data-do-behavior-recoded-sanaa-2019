38280cc40dac8e84844227b987c17a2387970ef8
learn <mohammedazizbuss@gmail.com>
2019-10-28 02:00:43 -0700

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
-
 /* Write your implementation of greet() */
+function greet(time){
+  let a= Number((time.split(":")[0]));
+  if(a<12){
+    return "Good Morning"
+  }
+  else if(a>=12&&a<=17){
+    return "Good Afternoon"
+  }
+  else if(a>17){
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(message){
+  document.getElementById("greeting").innerText=message;
+}
