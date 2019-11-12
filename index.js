314d97bb767b5c90b7a1c0e9dea95ce9cafab7ec
Sarah Alafif <sar1997yah@gmail.com>
2019-11-12 20:57:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function greet(e){
   
 }
 function displayMessage(mess){
-  let mes= document.getElementById("h1#greeting");
+  let mes= document.getElementById("greeting");
      mes.innerHTML= "Good";
 }
 /* End Given Code, don't edit above here...*/
